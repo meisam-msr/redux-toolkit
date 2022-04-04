2b5edf94b74898367e6878d532acd81d30237a2a
meisam <maghsoodiradmeisam12@gmail.com>
2022-04-04 18:44:48 +0430

    todosSlice added

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    { id: 1, title: "todo1", completed: false },
+    { id: 2, title: "todo2", completed: false },
+    { id: 3, title: "todo3", completed: true },
+  ],
+};
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: Date.now(),
+        title: action.payload.title,
+        completed: false,
+      };
+      state.todos.push(newTodo);
+    },
+  },
+});
+
+export const { addTodo } = todosSlice.actions;
+export default todosSlice.reducer;
