b5bbfa3f81824f52b9f6ee5a3595ccfa68ab3a38
meisam <maghsoodiradmeisam12@gmail.com>
2022-04-04 18:44:17 +0430

    add todo-starter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import "./App.css";
 import { Provider } from "react-redux";
 import { store } from "./features/store";
 import Counter from "./components/Counter";
+import AddTodoForm from "./components/Todos/AddTodoForm";
+import TodoList from "./components/Todos/TodoList";
+import TotalCompleteItems from "./components/Todos/TotalCompleteItems";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <Counter />
+        {/* <Counter /> */}
+        <AddTodoForm />
+        <TodoList />
+        <TotalCompleteItems />
       </div>
     </Provider>
   );
