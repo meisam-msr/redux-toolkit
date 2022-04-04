2878e248db6af9a127d562487ea699033bfbc357
meisam <maghsoodiradmeisam12@gmail.com>
2022-04-04 17:27:34 +0430

    payload added to actions

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,14 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const counterSlice = createSlice({
-  name: counter,
+  name: "counter",
   initialState: { value: 0 },
   reducers: {
     increment: (state, action) => {
-      state.value += 1;
+      state.value += action.payload;
     },
     decrement: (state, action) => {
-      state.value -= 1;
+      state.value -= action.payload;
     },
   },
 });
